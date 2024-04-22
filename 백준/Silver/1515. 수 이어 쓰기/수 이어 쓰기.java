e2af346e3334498bdc5eead8ee4d614cd5661421
DongJae <68364199+dkekzhs@users.noreply.github.com>
2024-04-22 23:30:32 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 100 ms, Memory: 13632 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.java
@@ -0,0 +1,30 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String s = br.readLine();
+        int check =0;
+        for (int i = 1; i <=30000 ; i++) {
+            String number = String.valueOf(i);
+
+            for (int j = 0; j < number.length(); j++) {
+                if(number.charAt(j) == s.charAt(check)) {
+                    check++;
+                }
+                if(check == s.length()){
+                    System.out.println(i);
+                    return;
+                }
+            }
+
+        }
+        System.out.println(check);
+
+    }
+
+}
+
